2eae268848d3acb8b056200048336cb8144665fe
Sara Enderborg <sara_enderborg@outlook.com>
2025-09-19 16:17:09 +0200

    This commit added hamburger menu and changed button styling

---
+++ script.js
@@ -0,0 +1,54 @@
+const hamburger = document.querySelector('.hamburger')
+const navbar = document.querySelector('.navbar')
+const navLinks = document.querySelectorAll('.navbar a')
+
+hamburger.addEventListener('click', () => {
+  navbar.classList.toggle('show')
+  hamburger.textContent = navbar.classList.contains('show') ? '✕' : '☰'
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navbar.classList.remove('show')
+    hamburger.textContent = '☰'
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
